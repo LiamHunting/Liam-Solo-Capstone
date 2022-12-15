33d9112fd4c12954c3d6fc057d87c3b7ee615ea1
LiamHunting <hunting.liam@gmail.com>
2022-12-14 17:42:53 -0700

    Project Update

---
+++ Liams_Project/LiamsTest.test.ts
@@ -0,0 +1,161 @@
+import { NinjaPage } from "./ninjaPage";
+import { until } from "selenium-webdriver";
+import { Driver } from "selenium-webdriver/chrome";
+
+const  ninjaPage = new NinjaPage();
+const fs = require('fs')
+
+class UserLogin {
+    email: string;
+    password: string;
+
+    constructor(email: string, password: string) {
+        this.email = email;
+        this.password = password;
+    };
+};
+
+
+let newUserLogin: Array<UserLogin> = [
+    new UserLogin("testdummy1@testing.com", "Password#1"),
+    new UserLogin("testdummy2@testing.com", "Password#2"),
+    new UserLogin("testdummy3@testing.com", "Password#3"),
+    new UserLogin("testdummy4@testing.com", "Password#4"),
+    new UserLogin("testdummy5@testing.com", "Password#5")
+]
+
+let login = async (newUserLogin) => {
+    await ninjaPage.click(ninjaPage.myAccountDropdown);
+    await ninjaPage.click(ninjaPage.loginPageBtn);
+    await ninjaPage.driver.wait(until.elementLocated(ninjaPage.emailInput));
+    await ninjaPage.click(ninjaPage.emailInput);
+    await ninjaPage.driver.findElement(ninjaPage.emailInput).sendKeys(newUserLogin.email);
+    console.log(newUserLogin.email);
+    await ninjaPage.click(ninjaPage.passwordInput);
+    await ninjaPage.driver.findElement(ninjaPage.passwordInput).sendKeys(newUserLogin.password);
+    console.log(newUserLogin.password);
+    await ninjaPage.click(ninjaPage.loginBtn);
+    await ninjaPage.driver.wait(until.elementLocated(ninjaPage.logoutBtn));
+    await ninjaPage.click(ninjaPage.logoutBtn);
+
+};
+
+
+describe("Testing suite for tutorialninja WebApp", () => {
+    beforeEach(async () => {
+        await ninjaPage.navigate();
+    });
+
+    afterAll(async () => {
+        await ninjaPage.driver.quit();
+    });
+
+    test("Currency Tab Euro", async () => {
+        await ninjaPage.click(ninjaPage.CurrencyTab);
+        await ninjaPage.click(ninjaPage.euroBtn);
+        await ninjaPage.click(ninjaPage.featMacBook);
+        let Euro = await ninjaPage.getText(ninjaPage.macPriceEuro);
+        expect(Euro).toContain("472.33€");
+        await ninjaPage.click(ninjaPage.homeBtn);
+        await ninjaPage.click(ninjaPage.CurrencyTab);
+        await ninjaPage.click(ninjaPage.poundBtn);
+        await ninjaPage.click(ninjaPage.featMacBook);
+        let Pound = await ninjaPage.getText(ninjaPage.macPricePound);
+        expect(Pound).toContain("£368.73");
+        await ninjaPage.click(ninjaPage.homeBtn);
+        await ninjaPage.click(ninjaPage.CurrencyTab);
+        await ninjaPage.click(ninjaPage.usDollarBtn);
+        await ninjaPage.click(ninjaPage.featMacBook);
+        let USDP = await ninjaPage.getText(ninjaPage.macPriceUSD);
+        expect(USDP).toContain("$602.00");
+    });
+
+    test("Dropdown Tabs With Screenshots", async () => {
+        await ninjaPage.click(ninjaPage.desktopsTab);
+        await ninjaPage.click(ninjaPage.desktopsTabShowAll);
+        await ninjaPage.driver.wait(until.elementLocated(ninjaPage.homeBtn));
+        await fs.writeFile(`${__dirname}/desktop.png`,
+         await ninjaPage.driver.takeScreenshot(), "base64",
+         (e) => {
+            if (e) console.error(e)
+            else console.log('Save Succesful')
+        });
+        await ninjaPage.click(ninjaPage.homeBtn);
+        await ninjaPage.click(ninjaPage.laptopsNotebookTab);
+        await ninjaPage.click(ninjaPage.laptopsNotebookTabShowAll);
+        await ninjaPage.driver.wait(until.elementLocated(ninjaPage.homeBtn));
+        await fs.writeFile(`${__dirname}/LaptopsNotebooks.png`,
+         await ninjaPage.driver.takeScreenshot(), "base64",
+         (e) => {
+            if (e) console.error(e)
+            else console.log('Save Succesful')
+        });
+        await ninjaPage.click(ninjaPage.homeBtn);
+        await ninjaPage.click(ninjaPage.componentsTab);
+        await ninjaPage.click(ninjaPage.componentsTabShowAll);
+        await ninjaPage.driver.wait(until.elementLocated(ninjaPage.homeBtn));
+        await fs.writeFile(`${__dirname}/components.png`,
+         await ninjaPage.driver.takeScreenshot(), "base64",
+         (e) => {
+            if (e) console.error(e)
+            else console.log('Save Succesful')
+        });
+        await ninjaPage.click(ninjaPage.homeBtn);
+        await ninjaPage.click(ninjaPage.tabletsTab);
+        await ninjaPage.driver.wait(until.elementLocated(ninjaPage.homeBtn));
+        await fs.writeFile(`${__dirname}/tablets.png`,
+         await ninjaPage.driver.takeScreenshot(), "base64",
+         (e) => {
+            if (e) console.error(e)
+            else console.log('Save Succesful')
+        });
+        await ninjaPage.click(ninjaPage.homeBtn);
+        await ninjaPage.click(ninjaPage.softwareTab);
+        await ninjaPage.driver.wait(until.elementLocated(ninjaPage.homeBtn));
+        await fs.writeFile(`${__dirname}/software.png`,
+         await ninjaPage.driver.takeScreenshot(), "base64",
+         (e) => {
+            if (e) console.error(e)
+            else console.log('Save Succesful')
+        });
+        await ninjaPage.click(ninjaPage.homeBtn);
+        await ninjaPage.click(ninjaPage.phonePDATab);
+        await ninjaPage.driver.wait(until.elementLocated(ninjaPage.homeBtn));
+        await fs.writeFile(`${__dirname}/phonesPDA.png`,
+         await ninjaPage.driver.takeScreenshot(), "base64",
+         (e) => {
+            if (e) console.error(e)
+            else console.log('Save Succesful')
+        });
+        await ninjaPage.click(ninjaPage.homeBtn);
+        await ninjaPage.click(ninjaPage.camerasTab);
+        await ninjaPage.driver.wait(until.elementLocated(ninjaPage.homeBtn));
+        await fs.writeFile(`${__dirname}/cameras.png`,
+         await ninjaPage.driver.takeScreenshot(), "base64",
+         (e) => {
+            if (e) console.error(e)
+            else console.log('Save Succesful')
+        });
+        await ninjaPage.click(ninjaPage.homeBtn);
+        await ninjaPage.click(ninjaPage.mp3Tab);
+        await ninjaPage.click(ninjaPage.mp3TabShowAll);
+        await ninjaPage.driver.wait(until.elementLocated(ninjaPage.homeBtn));
+        await fs.writeFile(`${__dirname}/mp3.png`,
+         await ninjaPage.driver.takeScreenshot(), "base64",
+         (e) => {
+            if (e) console.error(e)
+            else console.log('Save Succesful')
+        });
+        await ninjaPage.click(ninjaPage.homeBtn);
+        
+    });
+
+    test("Login Loop", async () => {
+        for(let i = 0; i < newUserLogin.length; i++) {
+            await login(newUserLogin[i]);
+        };
+    });
+
+
+    
+});
---
+++ Liams_Project/ninjaPage.ts
@@ -0,0 +1,43 @@
+import {BasePage} from "./basePage";
+import { By } from "selenium-webdriver";
+
+export class NinjaPage extends BasePage {
+    CurrencyTab: By = By.xpath('//button[@class="btn btn-link dropdown-toggle"]')
+    euroBtn: By = By.xpath('//button[text() = "€ Euro"]')
+    poundBtn: By = By.xpath('//button[text() = "£ Pound Sterling"]')
+    usDollarBtn: By = By.xpath('//button[text() = "$ US Dollar"]')
+    macPriceUSD: By = By.xpath('//h2[text() = "$602.00"]')
+    macPriceEuro: By = By.xpath('//h2[text() = "472.33€"]')
+    macPricePound: By = By.xpath('//h2[text() = "£368.73"]')
+
+    homeBtn: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=common/home"])[2]')
+
+    desktopsTab: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=20"])[1]')
+    desktopsTabShowAll: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=20"])[2]')
+    laptopsNotebookTab: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=18"])[1]')
+    laptopsNotebookTabShowAll: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=18"])[2]')
+    componentsTab: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=25"])[1]')
+    componentsTabShowAll: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=25"])[2]')
+    tabletsTab: By = By.xpath('//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=57"]')
+    softwareTab: By = By.xpath('//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=17"]')
+    phonePDATab: By = By.xpath('//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=24"]')
+    camerasTab: By = By.xpath('//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=33"]')
+    mp3Tab: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=34"])[1]')
+    mp3TabShowAll: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/category&path=34"])[2]')
+
+    myAccountDropdown: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=account/account"])[1]')
+    loginPageBtn: By = By.xpath('//a[@href="http://tutorialsninja.com/demo/index.php?route=account/login"]')
+    emailInput: By = By.xpath('//input[@name="email"]')
+    passwordInput: By = By.xpath('//input[@id="input-password"]')
+    loginBtn: By = By.xpath('//input[@value="Login"]')
+    logoutBtn: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=account/logout"])[2]')
+
+    featMacBook: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/product&product_id=43"])[1]')
+    featAppCin30: By = By.xpath('(//a[@href="http://tutorialsninja.com/demo/index.php?route=product/product&product_id=42"])[1]')
+
+
+    constructor() {
+        super({url: "http://tutorialsninja.com/demo/"})
+    };
+
+};
